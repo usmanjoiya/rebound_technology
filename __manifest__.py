4d710a61446199d8d7fb475a70017331b2f53ef5
mubarikakbar <mubarikakbar24@gmail.com>
2024-06-10 20:04:21 +0500

    update 10june

--- __manifest__.py
+++ __manifest__.py
@@ -23,6 +23,7 @@ Long description of module's purpose
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/footer.xml',
         'data/rebound_home.xml',
         'data/technology.xml',
         'data/sustainability.xml',
